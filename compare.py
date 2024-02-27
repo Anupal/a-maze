42b8aaaa5ce165ec4e02b4b9f83da98ef5b6527d
Anupal Mishra <anupal.pm@gmail.com>
2024-02-27 19:47:50 +0000

    additinal logging for comparison script

--- compare.py
+++ compare.py
@@ -1,47 +1,55 @@
 import maze
 import time
 import pandas as pd
-from memory_profiler import profile, memory_usage
-
-
-@profile
-def test_memory(func):
-    profile(func)()
+from memory_profiler import memory_usage
 
 
 def test_performance(func: callable):
+    print("      compute", flush=True)
     start_time = time.time()
     path, total_nodes, loops = func()
     end_time = time.time()
 
-    return end_time - start_time, len(path), len(total_nodes), loops
+    if not path:
+        print("     retrying...", flush=True)
+        return test_performance(func)
+
+    print("      memory", flush=True)
+    mem_usage, retval = memory_usage(func, interval=0.1, include_children=True, retval=True)
+
+    if not retval[0]:
+        print("     retrying...", flush=True)
+        return test_performance(func)
+
+    return end_time - start_time, len(path), len(total_nodes), loops, mem_usage
 
 
 def main():
-    test_maze_sizes = [i for i in range(5, 51, 5)]
+    test_maze_sizes = [i for i in range(50, 51, 5)]
 
     df, i = pd.DataFrame(columns=["maze_size", "algorithm", "exec_time", "path_length", "nodes_visited", "loops", "memory_min", "memory_max"]), 0
+    df["exec_time"] = df["exec_time"].astype(float)
 
     for test_size in test_maze_sizes:
-        print("Testing size ", test_size, flush=True)
+        print("Testing maze size =", test_size, flush=True)
         for k in range(3):
             print("  sample =", k, flush=True)
             test_maze = maze.Maze(test_size, test_size, 0.3)
             funcs = [test_maze.solve_dfs, test_maze.solve_bfs, test_maze.solve_astar, test_maze.solve_mdppit, test_maze.solve_mdpvit]
 
-            print("    ", end="", flush=True)
             for func in funcs:
                 algorithm = func.__name__.split("_")[1].upper()
-                print(algorithm, end=" ", flush=True)
-                tt, path, total_nodes, loops = test_performance(func)
-                mem_usage = memory_usage(func, interval=0.1, include_children=True)
+                print("   ", algorithm, flush=True)
+                tt, path, total_nodes, loops, mem_usage = test_performance(func)
 
-                df.loc[i] = [test_size, algorithm, round(tt, 4), path, total_nodes, loops, min(mem_usage), max(mem_usage)]
+                df.loc[i] = [test_size, algorithm, tt, path, total_nodes, loops, min(mem_usage), max(mem_usage)]
                 i += 1
-            print(flush=True)
 
             del test_maze
 
+    print("Collected results:")
+    print(df)
+    print("Saving to 'results.csv'")
     df.to_csv('results.csv', index=False)
 
 
